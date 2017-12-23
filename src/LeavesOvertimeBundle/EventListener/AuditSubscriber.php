04d93a8a91a52a3b743e167abf39b2f53433cfba
Rishikant Rampeara <rampeara@hotmail.com>
2017-12-23 05:16:14 +0400

    Standard edition + Sonata Admin Bundle + Sonata User Bundle and dependencies

---
+++ src/LeavesOvertimeBundle/EventListener/AuditSubscriber.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace LeavesOvertimeBundle\EventListener;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+
+class AuditSubscriber implements EventSubscriber
+{
+    private $validClasses = ['LeavesOvertimeBundle\Entity\Employee'];
+    private $context;
+    
+    public function __construct($securityContext){
+        $this->context= $securityContext;
+    }
+    
+    private function getUser(){
+        if ($this->context->getToken() != null) {
+            return $this->context->getToken()->getUser();
+        }
+        return null;
+    }
+    
+    public function isValidClass($object, $classNames)
+    {
+        foreach ($classNames as $className) {
+            if ($object instanceof $className) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public function getSubscribedEvents()
+    {
+        return array(
+            'prePersist',
+            'preUpdate',
+        );
+    }
+    
+    public function preUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+        if ($this->isValidClass($entity, $this->validClasses)) {
+            $entity->setUpdatedAt(new \DateTime());
+            $entity->setUpdatedBy($this->getUser());
+        }
+    }
+    
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+        if ($this->isValidClass($entity, $this->validClasses)) {
+            $entity->setCreatedAt(new \DateTime());
+            $entity->setCreatedBy($this->getUser());
+        }
+    }
+    
+}
